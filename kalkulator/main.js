074f20963ac8c853165d46ebb5438fbd520e2ff1
oktdib96 <oktdib96@gmail.com>
2021-02-07 13:11:08 +0100

    error krwa

--- kalkulator/main.js
+++ kalkulator/main.js
@@ -1,13 +1,40 @@
-const value1 = document.getElementById('v1');
-const value2 = document.getElementById('v2');
-const result = document.querySelector('button.result');
+
+const btn = document.querySelector('button.result');
 
 const wynik = () => {
+    const value1 = document.getElementById('v1');
+const value2 = document.getElementById('v2');
+    let myOperator;
+    let result;
 if(typeof value1.value === 'string' && typeof value2.value === 'string' || typeof value1.value === 'string' || typeof value2.value === 'string') {
     alert("Musisz podać wartości LICZBOWE!")
     value1.value ='';
     value2.value ='';
 }
+//pobranie operatorów
+const operators = document.getElementsByName('operator');
 
+for(let i = 0, numberOfOperators = operators.length; i < numberOfOperators; i++){
+if(operators[i].checked){
+    myOperator = operators[i].value;
+    
+    switch(operators[i].value) {
+        case "+":
+            result = value1 + value2;
+            break;
+            case "-":
+                result = value1 - value2;
+                break;
+                case "*":
+                result = value1 * value2;
+                break;
+                case "/":
+                    result = value1 / value2;
+                    break;
+
+    }
 }
-result.addEventListener('click', wynik);
+}
+console.log(result)
+document.querySelector("div.result").innerHTML = result}
+btn.addEventListener('click', wynik);
