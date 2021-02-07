0ec65199ca962cc63ea812afc3d2b81e4ad56e76
oktdib96 <oktdib96@gmail.com>
2021-02-07 13:27:07 +0100

    Gotowe

--- kalkulator/main.js
+++ kalkulator/main.js
@@ -6,12 +6,7 @@ const value1 = document.getElementById('v1');
 const value2 = document.getElementById('v2');
     let myOperator;
     let result;
-// if(typeof value1.value === 'string' && typeof value2.value === 'string' || typeof value1.value === 'string' || typeof value2.value === 'string') {
-//     alert("Musisz podać wartości LICZBOWE!")
-//     value1.value ='';
-//     value2.value ='';
-// }
-//pobranie operatorów
+
 const operators = document.getElementsByName('operator');
 
 for(let i = 0, numberOfOperators = operators.length; i < numberOfOperators; i++){
@@ -20,16 +15,16 @@ if(operators[i].checked){
     
     switch(operators[i].value) {
         case "+":
-            result = value1.value + value2.value;
+            result = parseInt(value1.value) + parseInt(value2.value);
             break;
             case "-":
-                result = value1.value - value2.value;
+                result = parseInt(value1.value) - parseInt(value2.value);
                 break;
                 case "*":
-                result = value1.value * value2.value;
+                result = parseInt(value1.value) * parseInt(value2.value);
                 break;
                 case "/":
-                    result = value1.value / value2.value;
+                    result = parseInt(value1.value) / parseInt(value2.value);
                     break;
 
     }
